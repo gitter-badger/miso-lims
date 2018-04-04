cc748cb8447267fca9867b6befd98c9866397a66
Noah Reinhardt <Noah.Reinhardt@oicr.on.ca>
2018-04-04 14:22:37 -0400

    Add endpoints and parameters for creating and editing workflows

--- miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/WorkflowController.java
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/WorkflowController.java
@@ -2,13 +2,22 @@ package uk.ac.bbsrc.tgac.miso.webapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping("/workflow")
 @Controller
 public class WorkflowController {
-  @RequestMapping("/workflow")
-  public ModelAndView editWorkflow(ModelMap model) {
+  @RequestMapping("/new/{workflowName}")
+  public ModelAndView createWorkflow(@PathVariable String workflowName, ModelMap model) {
+    model.put("title", "Load Sequencer Workflow");
+    model.put("message", "This is the user message describing what and how to input data into the input form.");
+    return new ModelAndView("/pages/workflow.jsp", model);
+  }
+
+  @RequestMapping("/edit/{id}")
+  public ModelAndView editWorkflow(@PathVariable long id, ModelMap model) {
     model.put("title", "Workflow title");
     model.put("message", "This is the user message describing what and how to input data into the input form.");
     return new ModelAndView("/pages/workflow.jsp", model);
---
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/WorkflowRestController.java
@@ -0,0 +1,11 @@
+package uk.ac.bbsrc.tgac.miso.webapp.controller.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rest/workflow")
+public class WorkflowRestController extends DefaultRestController {
+
+
+}
