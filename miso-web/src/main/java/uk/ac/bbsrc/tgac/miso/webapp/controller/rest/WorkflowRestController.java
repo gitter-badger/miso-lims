fae8f302709971159425d17d3b002be000c10b86
Noah Reinhardt <Noah.Reinhardt@oicr.on.ca>
2018-04-04 18:21:31 -0400

    Stub in WorkflowRestController

--- miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/WorkflowRestController.java
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/WorkflowRestController.java
@@ -2,10 +2,17 @@ package uk.ac.bbsrc.tgac.miso.webapp.controller.rest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import uk.ac.bbsrc.tgac.miso.core.data.workflow.WorkflowStepPrompt;
 
 @Controller
 @RequestMapping("/rest/workflow")
-public class WorkflowRestController extends DefaultRestController {
-
-
+public class WorkflowRestController extends RestController {
+  @RequestMapping(value = "/process", method = RequestMethod.POST)
+  public @ResponseBody WorkflowStepPrompt process(@RequestParam("input") String input) {
+    return new WorkflowStepPrompt(null, null);
+  }
 }
