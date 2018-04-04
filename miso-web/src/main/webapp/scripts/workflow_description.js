97fe7acb388f03eedc03ca63376b25fce6033f26
Noah Reinhardt <Noah.Reinhardt@oicr.on.ca>
2018-04-04 18:06:31 -0400

    Make a request to WorkflowRestController on enter

--- miso-web/src/main/webapp/scripts/workflow_description.js
+++ miso-web/src/main/webapp/scripts/workflow_description.js
@@ -1,7 +1,32 @@
 WorkflowDescription = (function() {
+  var processInput = function(input) {
+    var url = "/miso/rest/workflow/process";
+    var queryUrl = encodeURI(url + "/?" + jQuery.param({input: input}));
+
+    jQuery.ajax({
+      "dataType": "json",
+      "type": "POST",
+      "url": queryUrl,
+      "contentType": "application/json; charset=utf8",
+      "success": function(result) {
+        // todo
+        console.log(result);
+      },
+      "error": function() {
+        // todo: take parameters?
+      }
+    })
+  };
+
   return {
     initDescription: function(id, message) {
-      jQuery("#" + id).append("<p>" + message + "</p>").append("<input type='text'>");
+      var input = jQuery("<input/>").attr({type: "text"});
+      jQuery("#" + id).append("<p>" + message + "</p>").append(input);
+      input.keypress(function(e) {
+        if (e.which === 13) {
+          processInput(input.val());
+        }
+      })
     }
   }
 })();
